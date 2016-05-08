3a2e9898222ddcd2162479d344688d9676c6ac3d
Juan Mora <raicerk@gmail.com>
2016-05-08 13:44:37 -0300

    Se crea modelo para la ejecucion de Procedimiento almacenado con los parametros de entrada de usuario

---
+++ Login/Models/Home.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EXECSQLSERVERSP;
+using System.Data;
+
+namespace Login.Models
+{
+    public class Home
+    {
+        public string Cadena { get; set; }
+
+        public DataTable ListaContrasena() {
+            DataTable dt = new DataTable();
+            try
+            {
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                data.Add("@strCadena", this.Cadena);
+                dt = SQLSERVER.Query("login", "spRec_Login_ListaPalabras", data, dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return dt;
+            }
+        }
+    }
+}
